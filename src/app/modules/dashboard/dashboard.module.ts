140d78a572e8e8d1d5a00398eddfb5b8766773e7
alokikp@mindfiresolutions.com <alokikp@mindfiresolutions.com>
2020-05-10 03:54:46 +0530

    design responsive landing page

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardPage } from './pages/dashboard/dashboard.page';
 import { HomePage } from './pages/home/home.page';
+import { MaterialDesignModule } from './../../shared/modules/material-design/material-design.module';
 
 
 @NgModule({
   declarations: [DashboardPage, HomePage],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialDesignModule,
   ]
 })
 export class DashboardModule { }
