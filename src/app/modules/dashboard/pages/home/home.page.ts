dfc35aaa9093cd48093267a6fa64b1c253929cf2
alokikp@mindfiresolutions.com <alokikp@mindfiresolutions.com>
2020-05-25 14:25:29 +0530

    design plans section of landing page

--- src/app/modules/dashboard/pages/home/home.page.ts
+++ src/app/modules/dashboard/pages/home/home.page.ts
@@ -7,6 +7,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
+  pplans = [{name: 'Dedicated account managers to help you find and hire quality talent fast.'},
+    {name: 'Tailored search results featuring Top Rated and Rising Talent'},
+  ]
+
+  plans = [
+    { 
+      title: "Basic", 
+      action: "Select Basic", 
+      fee: "Free*", 
+      features: [ 
+        {name: 'Dedicated account managers to help you find and hire quality talent fast.'},
+        {name: 'Tailored search results featuring Top Rated and Rising Talent'},
+      ]
+   },
+   { 
+    title: "Plus", 
+    action: "Select Plus", 
+    fee: "$49.99/month", 
+    features: [ 
+      {name: 'Dedicated account managers to help you find and hire quality talent fast.'},
+      {name: 'Consolidated billing and invoicing with net 30 pay'},
+      {name: 'Tailored search results featuring Top Rated and Rising Talent'},
+    ]
+ },
+  { 
+    title: "Business", 
+    action: "Contact Us", 
+    fee: "$849/month**", 
+    features: [ 
+      {name: 'Dedicated account managers to help you find and hire quality talent fast.'},
+      {name: 'Consolidated billing and invoicing with net 30 pay'},
+      {name: 'Project tracking and collaboration tools for team'},
+      {name: 'Tailored search results featuring Top Rated and Rising Talent'},
+    ]
+  },
+  { 
+    title: "Enterprise", 
+    action: "Contact Us", 
+    fee: "Price varies as per user", 
+    features: [ 
+      {name: 'Dedicated account managers to help you find and hire quality talent fast.'},
+      {name: 'Consolidated billing and invoicing with net 30 pay'},
+      {name: 'Project tracking and collaboration tools for team'},
+      {name: 'Tailored search results featuring Top Rated and Rising Talent'},
+    ]
+  }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
