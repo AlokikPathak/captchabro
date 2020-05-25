4cabff82b302cb1577349e9b0b490d61d5a4cd69
alokikp@mindfiresolutions.com <alokikp@mindfiresolutions.com>
2020-05-25 16:25:37 +0530

    fix landing-page  header image

---
+++ src/app/modules/gameplay/gameplay.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { GameplayRoutingModule } from './gameplay-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    GameplayRoutingModule
+  ]
+})
+export class GameplayModule { }
