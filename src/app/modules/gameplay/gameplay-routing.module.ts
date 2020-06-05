249f35d5a30855228351f6acdc8d5c67653a7150
alokikp@mindfiresolutions.com <alokikp@mindfiresolutions.com>
2020-06-05 14:38:14 +0530

    link icons

--- src/app/modules/gameplay/gameplay-routing.module.ts
+++ src/app/modules/gameplay/gameplay-routing.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PlayFieldPage } from './pages/play-field/play-field.page';
+import { PaymentsPage } from './pages/payments/payments.page'
 
 
 const routes: Routes = [
   {
     path: '',
     component: PlayFieldPage
-  }
+  },
+  {
+    path: 'payments',
+    component: PaymentsPage
+  },
 ];
 
 @NgModule({
--- src/app/modules/gameplay/pages/payments/payments.page.ts
+++ src/app/modules/gameplay/pages/payments/payments.page.ts
@@ -7,6 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaymentsPage implements OnInit {
 
+  amount: number=0;
+
+  paymentList = 
+    [
+      {
+        to: "Paid to Rajesh Kumar", timestamp: "9:30 AM, 11 April, 2020", from: "Axis Bank", transactionId: "qeqdasfgad321423daf", 
+        amount: "Rs. 500", result:"SUCCESS", comments: "Transaction was successful. Booking ID: ADSLF324352SADFJSLXXXX234"
+      },
+      {
+        to: "Paid to SONU  Kumar", timestamp: "19:30 AM, 11 April, 2020", from: "Axis Bank", transactionId: "qeqdasfgad321423daf", 
+        amount: "Rs. 500", result:"SUCCESS", comments: "Transaction was successful. Booking ID: ADSLF324352SADFJSLXXXX234"
+      },
+      {
+        to: "Paid to KARAN Kumar", timestamp: "08:30 AM, 11 April, 2020", from: "Axis Bank", transactionId: "qeqdasfgad321423daf", 
+        amount: "Rs. 500", result:"SUCCESS", comments: "Transaction was successful. Booking ID: ADSLF324352SADFJSLXXXX234"
+      },
+      {
+        to: "Paid to Rakesh Kumar", timestamp: "10:30 PM, 11 April, 2020", from: "Axis Bank", transactionId: "qeqdasfgad321423daf", 
+        amount: "Rs. 500", result:"SUCCESS", comments: "Transaction was successful. Booking ID: ADSLF324352SADFJSLXXXX234"
+      },
+      {
+        to: "Paid to Rajesh Kumar", timestamp: "9:30 AM, 11 April, 2020", from: "Axis Bank", transactionId: "qeqdasfgad321423daf", 
+        amount: "Rs. 500", result:"SUCCESS", comments: "Transaction was successful. Booking ID: ADSLF324352SADFJSLXXXX234"
+      },
+      {
+        to: "Paid to Rajesh Kumar", timestamp: "9:30 AM, 11 April, 2020", from: "Axis Bank", transactionId: "qeqdasfgad321423daf", 
+        amount: "Rs. 500", result:"SUCCESS", comments: "Transaction was successful. Booking ID: ADSLF324352SADFJSLXXXX234"
+      },
+    ];
+
+
   constructor() { }
 
   ngOnInit(): void {
