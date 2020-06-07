e8e74cc4da43100cdf394427186ab6127f55e7e3
AlokikPathak <alokik2009@gmail.com>
2020-06-07 15:19:59 +0530

    redesign layouts

--- src/app/modules/gameplay/pages/payments/payments.page.ts
+++ src/app/modules/gameplay/pages/payments/payments.page.ts
@@ -13,27 +13,27 @@ export class PaymentsPage implements OnInit {
     [
       {
         to: "Paid to Rajesh Kumar", timestamp: "9:30 AM, 11 April, 2020", from: "Axis Bank", transactionId: "qeqdasfgad321423daf", 
-        amount: "Rs. 500", result:"SUCCESS", comments: "Transaction was successful. Booking ID: ADSLF324352SADFJSLXXXX234"
+        amount: "500", result:"SUCCESS", comments: "Transaction was successful. Booking ID: ADSLF324352SADFJSLXXXX234"
       },
       {
-        to: "Paid to SONU  Kumar", timestamp: "19:30 AM, 11 April, 2020", from: "Axis Bank", transactionId: "qeqdasfgad321423daf", 
-        amount: "Rs. 500", result:"SUCCESS", comments: "Transaction was successful. Booking ID: ADSLF324352SADFJSLXXXX234"
+        to: "Paid to SONU  Kumar", timestamp: "19:30 AM, 11 April, 2020", from: "State Bank Of India", transactionId: "qeqdasfgad321423daf", 
+        amount: "7500", result:"SUCCESS", comments: "Transaction was successful. Booking ID: ADSLF324352SADFJSLXXXX234"
       },
       {
-        to: "Paid to KARAN Kumar", timestamp: "08:30 AM, 11 April, 2020", from: "Axis Bank", transactionId: "qeqdasfgad321423daf", 
-        amount: "Rs. 500", result:"SUCCESS", comments: "Transaction was successful. Booking ID: ADSLF324352SADFJSLXXXX234"
+        to: "Paid to KARAN Kumar", timestamp: "08:30 AM, 11 April, 2020", from: "YES Bank", transactionId: "qeqdasfgad321423daf", 
+        amount: "5100", result:"SUCCESS", comments: "Transaction was successful. Booking ID: ADSLF324352SADFJSLXXXX234"
       },
       {
         to: "Paid to Rakesh Kumar", timestamp: "10:30 PM, 11 April, 2020", from: "Axis Bank", transactionId: "qeqdasfgad321423daf", 
-        amount: "Rs. 500", result:"SUCCESS", comments: "Transaction was successful. Booking ID: ADSLF324352SADFJSLXXXX234"
+        amount: "800", result:"SUCCESS", comments: "Transaction was successful. Booking ID: ADSLF324352SADFJSLXXXX234"
       },
       {
         to: "Paid to Rajesh Kumar", timestamp: "9:30 AM, 11 April, 2020", from: "Axis Bank", transactionId: "qeqdasfgad321423daf", 
-        amount: "Rs. 500", result:"SUCCESS", comments: "Transaction was successful. Booking ID: ADSLF324352SADFJSLXXXX234"
+        amount: "8100", result:"SUCCESS", comments: "Transaction was successful. Booking ID: ADSLF324352SADFJSLXXXX234"
       },
       {
         to: "Paid to Rajesh Kumar", timestamp: "9:30 AM, 11 April, 2020", from: "Axis Bank", transactionId: "qeqdasfgad321423daf", 
-        amount: "Rs. 500", result:"SUCCESS", comments: "Transaction was successful. Booking ID: ADSLF324352SADFJSLXXXX234"
+        amount: "9300", result:"SUCCESS", comments: "Transaction was successful. Booking ID: ADSLF324352SADFJSLXXXX234"
       },
     ];
 
